c802853591991e0eda5433235704c4b3bcdf0b3c
petayyyy <ilyaholodilov19072002@gmail.com>
2026-03-13 15:33:21 +0300

    Update for work  on localhost

--- app/run.sh
+++ app/run.sh
@@ -17,10 +17,16 @@ fi
 
 if [ -n "${USE_PROXY}" ]; then
 	echo "Use Proxy!"
+
+	WS_ORIGIN_VALUE=${BOKEH_ALLOW_WS_ORIGIN:-${DOMAIN_VALUE}}
+	if [ "${WS_ORIGIN_VALUE}" = "0.0.0.0" ] || [ "${WS_ORIGIN_VALUE}" = "*" ]; then
+		WS_ORIGIN_VALUE="*"
+	fi
+
 	python3 ${WORK_PATH}/serve.py \
 		--port=${PORT_VALUE} \
 		--address=0.0.0.0 \
-		--allow-websocket-origin=${DOMAIN_VALUE} \
+		--allow-websocket-origin=${WS_ORIGIN_VALUE} \
 		--use-xheaders
 else
 	python3 ${WORK_PATH}/serve.py --port=${PORT_VALUE}
